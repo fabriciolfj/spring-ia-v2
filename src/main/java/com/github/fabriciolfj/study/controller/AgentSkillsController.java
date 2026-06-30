7edf590a8b43ac8946e71ab906d4b9d5646e54a3
fabricio <fabricio.jacob@outlook.com>
2026-06-30 20:51:31 -0300

    feat: init project.

--- src/main/java/com/github/fabriciolfj/study/controller/AgentSkillsController.java
+++ src/main/java/com/github/fabriciolfj/study/controller/AgentSkillsController.java
@@ -3,7 +3,9 @@ package com.github.fabriciolfj.study.controller;
 import com.github.fabriciolfj.study.dto.ReportRequest;
 import com.github.fabriciolfj.study.model.AnthropicDocument;
 import com.github.fabriciolfj.study.service.AgentSkillsService;
+import com.github.fabriciolfj.study.service.OrchestratorService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -14,14 +16,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/agent-skills")
 @Validated
 public class AgentSkillsController {
     private final AgentSkillsService agentSkillsService;
+    private final OrchestratorService orchestratorService;
 
-    public AgentSkillsController(AgentSkillsService agentSkillsService) {
+    public AgentSkillsController(AgentSkillsService agentSkillsService, OrchestratorService orchestratorService) {
         this.agentSkillsService = agentSkillsService;
+        this.orchestratorService = orchestratorService;
+    }
+
+    @GetMapping
+    public void requestSubAgents() {
+        var result = orchestratorService.ask("""
+                    Perform the following tasks:
+                    - Review the code quality example.
+                    - Generate concise technical documentation like user guide.
+                    """);
+
+        log.info(result);
     }
 
     @GetMapping("/report")
